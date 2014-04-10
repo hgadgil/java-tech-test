13a9b5effb2ebe9f62805661f18faa921c603beb
Harshawardhan Gadgil <hgadgil@vmware.com>
2014-04-10 16:21:47 -0700

    Created base Maven project

---
+++ src/test/java/net/hgadgil/javatechtest/SimpleTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package net.hgadgil.javatechtest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author hgadgil
+ *
+ */
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		assertEquals(100, 100);
+	}
+
+}
